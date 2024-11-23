43bd8a5171a7abe7659cb76c832213ac05ba247c
slckerdogan <108437150+slckerdogan@users.noreply.github.com>
2024-11-24 00:24:14 +0300

    Shopping Center
    Shopping Center

---
+++ shoppingCenter/src/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div style={{textAlign:"center", verticalAlign:"center",marginTop:"150px",fontSize:"50px"}}>Loading..............</div>
+  )
+}
+
+export default Loading
